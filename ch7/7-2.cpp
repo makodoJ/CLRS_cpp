7fd35212756c20ec79eacfe17381aa093ad6fc1a
makodoJ <69521116+makodoJ@users.noreply.github.com>
2020-12-08 20:40:37 +0800

    Add files via upload

---
+++ ch7/7-2.cpp
@@ -0,0 +1,18 @@
+//
+// Created by makodo on 2020/12/8.
+//
+
+#include "7-2.h"
+#include "/Users/jiangrenjie/Documents/clrs/print/print.h"
+
+using namespace CLRS::CH7;
+
+int main(){
+    cout << "Initialize an array of A\n";
+    vector<int> A = {13, 19, 9, 5, 12, 8, 7, 4, 11, 2, 6, 21};
+    print(A);
+
+    cout << "\nPerform randomized_quicksort_equal(A, 0, 11)\n";
+    randomized_quicksort_equal(A, 0, 11);
+    print(A);
+}
---
+++ ch7/7-2.h
@@ -0,0 +1,54 @@
+//
+// Created by makodo on 2020/12/8.
+//
+
+#ifndef prob7_2_h
+#define prob7_2_h
+
+#include "/Users/jiangrenjie/Documents/clrs/ch5/5.1.h"
+#include "7.1.h"
+
+namespace CLRS{namespace CH7{
+    // - Time : O(n)
+    template <typename T>
+    vector<int> partition_equal(vector<T>& A, int p, int r){
+        T x = A[r];
+        int q = p;
+        int t = p;
+        int i;
+        for(i = p + 1; i <= r; i++){
+            if(A[i] < x){
+                int y = A[i];
+                A[i] = A[t + 1];
+                A[q] = y;
+                ++q;
+                ++t;
+            }
+            if(A[i] == x){
+                swap(A[t + 1], A[i]);
+                ++t;
+            }
+        }
+        return { q, t };
+    }
+
+    // - Time : O(n)
+    template <typename T>
+    vector<int> randomized_partition_equal(vector<T>& A, int p, int r){
+        int i = CH5::random(p, r);
+        swap(A[r], A[i]);
+        return partition_equal(A, p, r);
+    }
+
+    // - Time : O(nlgn)
+    template <typename T>
+    void randomized_quicksort_equal(vector<T>& A, int p, int r){
+        if(p < r){
+            vector<int> a = randomized_partition_equal(A, p, r);
+            randomized_quicksort_equal(A, p, a[0] - 1);
+            randomized_quicksort_equal(A, a[1] + 1, r);
+        }
+    }
+}}
+
+#endif
